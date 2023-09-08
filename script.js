fdaf1f000a0aa141a93c6e3d6bb353a9be5ea0e3
Riti <112542247+sinhariti@users.noreply.github.com>
2023-09-08 15:01:19 +0530

    js

---
+++ script.js
@@ -0,0 +1,10 @@
+//navigation bar
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar_menu');
+
+// Display Mobile menu
+const mobileMenu =()=>{
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+}
+menu.addEventListener('click',mobileMenu);
